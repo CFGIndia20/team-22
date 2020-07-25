1d1945a57a9fec949c924e01e96e2c44c5908704
Kaustubh Kekre <kkekre90@gmail.com>
2020-07-25 14:25:40 +0530

    add android studio project

---
+++ android/Umeed/app/src/main/java/com/markups/umeed/MainActivity.java
@@ -0,0 +1,26 @@
+package com.markups.umeed;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences spref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spref = getApplicationContext().getSharedPreferences("user", MODE_PRIVATE);
+        if(spref.getString("type","init").equals("init")){
+            Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else
+        {
+            startActivity(new Intent(MainActivity.this, BottomNavigationMain.class));
+            finish();
+        }
+    }
+}
