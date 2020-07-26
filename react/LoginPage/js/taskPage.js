42aa03579704b01eeb596d96330ca63195dab620
Aman-Arcanion9 <amannnk27@gmail.com>
2020-07-26 10:32:32 +0530

    Admin Workspace

--- react/LoginPage/js/taskPage.js
+++ react/LoginPage/js/taskPage.js
@@ -21,3 +21,24 @@ function date() {
   document.getElementById("startdate").setAttribute("min", today);
   document.getElementById("enddate").setAttribute("min", enddate);
 }
+
+function rowCreate(){
+  var table = document.getElementById("eventTable");
+
+// Create an empty <tr> element and add it to the 1st position of the table:
+var row = table.insertRow(0);
+
+// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+var cell4 = row.insertCell(3);
+var cell5 = row.insertCell(4);
+
+// Add some text to the new cells:
+cell1.innerHTML = "NEW CELL1";
+cell2.innerHTML = "NEW CELL2";
+cell3.innerHTML = "NEW CELL1";
+cell4.innerHTML = "NEW CELL2";
+cell5.innerHTML = "NEW CELL1";
+}
